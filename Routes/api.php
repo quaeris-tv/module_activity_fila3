b1e4e3a8934ed533744eee40641a59e844070334
marco76tv <marco76tv@users.noreply.github.com>
2023-10-27 21:38:02 +0000

    Lint

--- Routes/api.php
+++ Routes/api.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/activity', function (Request $request) {//
+// Route::middleware('auth:api')->get('/activity', function (Request $request) {//
 //    return $request->user();
-//});
+// });
