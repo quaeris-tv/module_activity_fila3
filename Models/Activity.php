f1b7d27f321ea4b34a31813dc70576128976423d
marco76tv <marco.sottana@gmail.com>
2023-11-03 12:13:15 +0100

    up

--- Models/Activity.php
+++ Models/Activity.php
@@ -12,29 +12,29 @@ use Spatie\Activitylog\Models\Activity as SpatieActivity;
 /**
  * Modules\Activity\Models\Activity.
  *
- * @property int             $id
- * @property string|null     $log_name
- * @property string          $description
- * @property string|null     $subject_type
- * @property string|null     $event
- * @property int|null        $subject_id
- * @property string|null     $causer_type
- * @property int|null        $causer_id
+ * @property int $id
+ * @property string|null $log_name
+ * @property string $description
+ * @property string|null $subject_type
+ * @property int|null $subject_id
+ * @property string|null $causer_type
+ * @property int|null $causer_id
  * @property Collection|null $properties
- * @property string|null     $batch_uuid
- * @property Carbon|null     $created_at
- * @property Carbon|null     $updated_at
- * @property Model|\Eloquent $causer
- * @property Collection      $changes
- * @property Model|\Eloquent $subject
- * @property string|null     $field_name
- *
- * @method static Builder|Activity                               causedBy(Model $causer)
- * @method static Builder|Activity                               forBatch(string $batchUuid)
- * @method static Builder|Activity                               forEvent(string $event)
- * @method static Builder|Activity                               forSubject(Model $subject)
- * @method static Builder|Activity                               hasBatch()
- * @method static Builder|Activity                               inLog(...$logNames)
+ * @property string|null $batch_uuid
+ * @property string|null $event
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property-read Model|\Eloquent $causer
+ * @property-read Collection $changes
+ * @property-read Model|\Eloquent $subject
+ * @method static Builder|Activity causedBy(\Illuminate\Database\Eloquent\Model $causer)
+ * @method static Builder|Activity forBatch(string $batchUuid)
+ * @method static Builder|Activity forEvent(string $event)
+ * @method static Builder|Activity forSubject(\Illuminate\Database\Eloquent\Model $subject)
+ * @method static Builder|Activity hasBatch()
+ * @method static Builder|Activity inLog(...$logNames)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Activity newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Activity query()
@@ -42,6 +42,7 @@ use Spatie\Activitylog\Models\Activity as SpatieActivity;
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCauserId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCauserType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCreatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereDescription($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereEvent($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereId($value)
@@ -50,13 +51,7 @@ use Spatie\Activitylog\Models\Activity as SpatieActivity;
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereSubjectId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereSubjectType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereUpdatedAt($value)
- *
- * @property string|null $created_by
- * @property string|null $updated_by
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCreatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereUpdatedBy($value)
- *
  * @mixin \Eloquent
  */
 class Activity extends SpatieActivity
--- Models/StoredEvent.php
+++ Models/StoredEvent.php
@@ -13,39 +13,40 @@ use Spatie\SchemalessAttributes\SchemalessAttributes;
 /**
  * Modules\Activity\Models\StoredEvent.
  *
- * @property ShouldBeStored|null  $event
+ * @property int $id
+ * @property string|null $aggregate_uuid
+ * @property int|null $aggregate_version
+ * @property int $event_version
+ * @property string $event_class
+ * @property array $event_properties
  * @property SchemalessAttributes $meta_data
- *
- * @method static EloquentStoredEventQueryBuilder|StoredEvent         afterVersion(int $version)
- * @method static EloquentStoredEventCollection<int, static>          all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          get($columns = ['*'])
- * @method static EloquentStoredEventQueryBuilder|StoredEvent         lastEvent(string ...$eventClasses)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent         newModelQuery()
- * @method static EloquentStoredEventQueryBuilder|StoredEvent         newQuery()
- * @method static EloquentStoredEventQueryBuilder|StoredEvent         query()
- * @method static EloquentStoredEventQueryBuilder|StoredEvent         startingFrom(int $storedEventId)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent         whereAggregateRoot(string $uuid)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent         whereEvent(string ...$eventClasses)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent         wherePropertyIs(string $property, ?mixed $value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent         wherePropertyIsNot(string $property, ?mixed $value)
- * @method static EloquentStoredEventQueryBuilder|EloquentStoredEvent withMetaDataAttributes()
- * @method static EloquentStoredEventCollection<int, static>          all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>          get($columns = ['*'])
- *
+ * @property string $created_at
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property-read \Spatie\EventSourcing\StoredEvents\ShouldBeStored|null $event
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent afterVersion(int $version)
+ * @method static EloquentStoredEventCollection<int, static> all($columns = ['*'])
+ * @method static EloquentStoredEventCollection<int, static> get($columns = ['*'])
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent lastEvent(string ...$eventClasses)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent newModelQuery()
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent newQuery()
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent query()
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent startingFrom(int $storedEventId)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereAggregateRoot(string $uuid)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereAggregateUuid($value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereAggregateVersion($value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereCreatedAt($value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereCreatedBy($value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereEvent(string ...$eventClasses)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereEventClass($value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereEventProperties($value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereEventVersion($value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereId($value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereMetaData($value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIs(string $property, ?mixed $value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIsNot(string $property, ?mixed $value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereUpdatedBy($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|EloquentStoredEvent withMetaDataAttributes()
  * @mixin \Eloquent
  */
 class StoredEvent extends SpatieStoredEvent
