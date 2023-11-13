488750715c4772e19c21a10a2fee56d4f5ed54af
marco76tv <marco.sottana@gmail.com>
2023-11-13 16:39:34 +0100

    up

--- Models/Activity.php
+++ Models/Activity.php
@@ -14,7 +14,6 @@ use Spatie\Activitylog\Models\Activity as SpatieActivity;
  * @property Model|\Eloquent $causer
  * @property Collection      $changes
  * @property Model|\Eloquent $subject
- *
  * @method static Builder|Activity                               causedBy(\Illuminate\Database\Eloquent\Model $causer)
  * @method static Builder|Activity                               forBatch(string $batchUuid)
  * @method static Builder|Activity                               forEvent(string $event)
@@ -24,7 +23,30 @@ use Spatie\Activitylog\Models\Activity as SpatieActivity;
  * @method static \Illuminate\Database\Eloquent\Builder|Activity newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Activity newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Activity query()
- *
+ * @property int $id
+ * @property string|null $log_name
+ * @property string $description
+ * @property string|null $subject_id
+ * @property string|null $subject_type
+ * @property string|null $event
+ * @property string|null $causer_type
+ * @property string|null $causer_id
+ * @property Collection|null $properties
+ * @property string|null $batch_uuid
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereBatchUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCauserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCauserType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereEvent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereLogName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereProperties($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereSubjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereSubjectType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Activity extends SpatieActivity
