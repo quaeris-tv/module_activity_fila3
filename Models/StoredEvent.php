46b129a164b1517af41a408643fddd4add69d5ff
marco76tv <marco76tv@users.noreply.github.com>
2023-11-14 12:46:09 +0000

    Lint

--- Models/StoredEvent.php
+++ Models/StoredEvent.php
@@ -14,6 +14,7 @@ use Spatie\SchemalessAttributes\SchemalessAttributes;
  *
  * @property \Spatie\EventSourcing\StoredEvents\ShouldBeStored|null $event
  * @property SchemalessAttributes                                   $meta_data
+ *
  * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   afterVersion(int $version)
  * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
  * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
@@ -29,8 +30,9 @@ use Spatie\SchemalessAttributes\SchemalessAttributes;
  * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|EloquentStoredEvent withMetaDataAttributes()
  * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
  * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static> all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static> get($columns = ['*'])
+ * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
+ * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
+ *
  * @mixin \Eloquent
  */
 class StoredEvent extends SpatieStoredEvent
