6838c783b8a216ef466af0a68d803e22df85d9e4
marco76tv <marco.sottana@gmail.com>
2023-11-23 18:01:44 +0100

    Update model annotations and method signatures<commit message> Update model annotations and method signatures

--- Models/StoredEvent.php
+++ Models/StoredEvent.php
@@ -12,23 +12,21 @@ use Spatie\SchemalessAttributes\SchemalessAttributes;
 /**
  * Modules\Activity\Models\StoredEvent.
  *
- * @property \Spatie\EventSourcing\StoredEvents\ShouldBeStored|null $event
- * @property SchemalessAttributes                                   $meta_data
- *
- * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   afterVersion(int $version)
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   lastEvent(string ...$eventClasses)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   newModelQuery()
- * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   newQuery()
- * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   query()
- * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   startingFrom(int $storedEventId)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   whereAggregateRoot(string $uuid)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   whereEvent(string ...$eventClasses)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   wherePropertyIs(string $property, ?mixed $value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   wherePropertyIsNot(string $property, ?mixed $value)
+ * @property-read \Spatie\EventSourcing\StoredEvents\ShouldBeStored|null $event
+ * @property-read SchemalessAttributes $meta_data
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent afterVersion(int $version)
+ * @method static EloquentStoredEventCollection<int, static> all($columns = ['*'])
+ * @method static EloquentStoredEventCollection<int, static> get($columns = ['*'])
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent lastEvent(string ...$eventClasses)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent newModelQuery()
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent newQuery()
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent query()
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent startingFrom(int $storedEventId)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereAggregateRoot(string $uuid)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent whereEvent(string ...$eventClasses)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIs(string $property, ?mixed $value)
+ * @method static EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIsNot(string $property, ?mixed $value)
  * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|EloquentStoredEvent withMetaDataAttributes()
- *
  * @mixin \Eloquent
  */
 class StoredEvent extends SpatieStoredEvent
