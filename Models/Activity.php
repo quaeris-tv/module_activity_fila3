b2a0d0818c556c29ea45b5220f851c8c665e8497
Malebestia <vair81@gmail.com>
2023-11-23 12:14:10 +0100

    up

--- Models/Activity.php
+++ Models/Activity.php
@@ -11,48 +11,18 @@ use Spatie\Activitylog\Models\Activity as SpatieActivity;
 /**
  * Modules\Activity\Models\Activity.
  *
- * @property int                             $id
- * @property string|null                     $log_name
- * @property string                          $description
- * @property string|null                     $subject_type
- * @property int|null                        $subject_id
- * @property string|null                     $causer_type
- * @property int|null                        $causer_id
- * @property Collection|null                 $properties
- * @property string|null                     $batch_uuid
- * @property string|null                     $event
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null                     $created_by
- * @property string|null                     $updated_by
- * @property Model|\Eloquent                 $causer
- * @property Collection                      $changes
- * @property Model|\Eloquent                 $subject
- *
- * @method static Builder|Activity                               causedBy(\Illuminate\Database\Eloquent\Model $causer)
- * @method static Builder|Activity                               forBatch(string $batchUuid)
- * @method static Builder|Activity                               forEvent(string $event)
- * @method static Builder|Activity                               forSubject(\Illuminate\Database\Eloquent\Model $subject)
- * @method static Builder|Activity                               hasBatch()
- * @method static Builder|Activity                               inLog(...$logNames)
+ * @property-read Model|\Eloquent $causer
+ * @property-read Collection $changes
+ * @property-read Model|\Eloquent $subject
+ * @method static Builder|Activity causedBy(\Illuminate\Database\Eloquent\Model $causer)
+ * @method static Builder|Activity forBatch(string $batchUuid)
+ * @method static Builder|Activity forEvent(string $event)
+ * @method static Builder|Activity forSubject(\Illuminate\Database\Eloquent\Model $subject)
+ * @method static Builder|Activity hasBatch()
+ * @method static Builder|Activity inLog(...$logNames)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Activity newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Activity query()
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereBatchUuid($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCauserId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCauserType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereEvent($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereLogName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereProperties($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereSubjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereSubjectType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Activity whereUpdatedBy($value)
- *
  * @mixin \Eloquent
  */
 class Activity extends SpatieActivity
