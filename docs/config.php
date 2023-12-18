b7e57354ef775a35d93cbcbfe0f535b0a041712c
marco76tv <marco.sottana@gmail.com>
2023-12-18 16:12:10 +0100

    rebase 400

--- docs/config.php
+++ docs/config.php
@@ -15,7 +15,7 @@ return [
     'docsearchIndexName' => env('DOCSEARCH_INDEX'),
 
     // navigation menu
-    'navigation' => require_once ('navigation.php'),
+    'navigation' => require_once('navigation.php'),
 
     // helpers
     'isActive' => function ($page, $path) {
@@ -31,6 +31,7 @@ return [
     'url' => function ($page, $path) {
         return Str::startsWith($path, 'http') ? $path : $page->baseUrl.'/'.trimPath($path);
     },
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -372,3 +373,6 @@ return [
 =======
 ];
 >>>>>>> 8edebcf (rebase 13/143)
+=======
+];
+>>>>>>> 89f62b5 (Dusting)
