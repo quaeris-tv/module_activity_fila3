8469eda6480f3e4992d6f801dd56f585082ff524
marco76tv <marco.sottana@gmail.com>
2023-12-18 16:27:54 +0100

    conflict

--- docs/config.production.php
+++ docs/config.production.php
@@ -9,40 +9,4 @@ return [
     // DocSearch credentials
     'docsearchApiKey' => env('DOCSEARCH_KEY'),
     'docsearchIndexName' => env('DOCSEARCH_INDEX'),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 ];
-=======
-];
->>>>>>> b4eb4c6 (.)
-=======
-];
->>>>>>> e1c77f7 (Lint)
-=======
-];
->>>>>>> b4eb4c6 (.)
-=======
-];
->>>>>>> e1c77f7 (Lint)
-=======
-];
->>>>>>> ff79a66 (Remove unnecessary files and directories)
-=======
-=======
->>>>>>> 4097ac6 (Fix navigation require_once statement in config.php files)
-];
-=======
-];
->>>>>>> 285fa96 (.)
-<<<<<<< HEAD
->>>>>>> 0946dfa (Update navigation.php and config.php files)
-=======
-=======
-];
->>>>>>> d019e5d (Lint)
->>>>>>> 4097ac6 (Fix navigation require_once statement in config.php files)
