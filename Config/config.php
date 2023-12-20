623bdc56e90c1199e9caede63e55fd907fe249da
marco76tv <marco.sottana@gmail.com>
2023-12-20 16:38:17 +0100

    up

--- Config/config.php
+++ Config/config.php
@@ -4,12 +4,5 @@ declare(strict_types=1);
 
 return [
     'name' => 'Activity',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     'navigation_sort' => 1,
->>>>>>> dev
-=======
-    'navigation_sort' => 1,
->>>>>>> dev
 ];
