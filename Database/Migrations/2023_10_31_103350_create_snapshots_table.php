4a0f17864f93d6869dfa0a22441057372d4b2fbe
Malebestia <vair81@gmail.com>
2023-12-22 10:10:31 +0100

    first

---
+++ Database/Migrations/2023_10_31_103350_create_snapshots_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+
+class CreateSnapshotsTable extends XotBaseMigration
+{
+    public function up(): void
+    {
+        // -- CREATE --
+        $this->tableCreate(
+            function (Blueprint $table): void {
+                $table->bigIncrements('id');
+                $table->uuid('aggregate_uuid');
+                $table->unsignedInteger('aggregate_version');
+                $table->jsonb('state');
+                $table->timestamps();
+                $table->index('aggregate_uuid');
+            }
+        );
+        // -- UPDATE --
+        $this->tableUpdate(
+            function (Blueprint $table): void {
+                if (! $this->hasColumn('created_by')) {
+                    $table->string('created_by')->nullable();
+                    $table->string('updated_by')->nullable();
+                }
+            }
+        );
+    }
+}
---
+++ Filament/Resources/ActivityResource.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Activity\Filament\Resources;
+
+use Filament\Forms\Form;
+use Filament\Resources\Pages\PageRegistration;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Table;
+use Modules\Activity\Filament\Resources\ActivityResource\Pages\CreateActivity;
+use Modules\Activity\Filament\Resources\ActivityResource\Pages\EditActivity;
+use Modules\Activity\Filament\Resources\ActivityResource\Pages\ListActivities;
+use Modules\Activity\Models\Activity;
+
+class ActivityResource extends Resource
+{
+    protected static ?string $model = Activity::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+            ])
+            ->filters([
+            ])
+            ->actions([
+                EditAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                // {{ tableEmptyStateActions }}
+            ]);
+    }
+
+    /**
+     * @psalm-return array<never, never>
+     */
+    public static function getRelations(): array
+    {
+        return [
+        ];
+    }
+
+    /**
+     * @return array<string, PageRegistration>
+     */
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListActivities::route('/'),
+            'create' => CreateActivity::route('/create'),
+            'edit' => EditActivity::route('/{record}/edit'),
+        ];
+    }
+}
