39d5b5de0f6a7bfe42cdfccf57904636dac68155
marco76tv <marco.sottana@gmail.com>
2024-01-09 11:16:19 +0100

    Resolve conflict in .gitignore and config.php files

--- Config/config.php
+++ Config/config.php
@@ -6,9 +6,13 @@ return [
     'name' => 'Activity',
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     'navigation_sort' => 1,
 =======
 >>>>>>> dev
 =======
 >>>>>>> 0777dc9 (first)
+=======
+    'navigation_sort' => 1,
+>>>>>>> 6edc7c2 (conflict)
 ];
