4a5fd7710ea8822ebdf4a0b5157ec8131a6931ba
marco76tv <marco.sottana@gmail.com>
2024-01-09 11:26:41 +0100

    up

--- Config/config.php
+++ Config/config.php
@@ -4,15 +4,5 @@ declare(strict_types=1);
 
 return [
     'name' => 'Activity',
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     'navigation_sort' => 1,
-=======
->>>>>>> dev
-=======
->>>>>>> 0777dc9 (first)
-=======
-    'navigation_sort' => 1,
->>>>>>> 6edc7c2 (conflict)
 ];
