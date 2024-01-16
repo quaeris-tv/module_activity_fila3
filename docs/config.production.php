8cb09c28c17a2921bdeaf2bf1f47087b98ad7107
marco76tv <marco.sottana@gmail.com>
2024-01-16 08:53:45 +0100

    rebase 1

---
+++ docs/config.production.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'baseUrl' => 'https://laraxot.github.io/doc_activity',
+    'production' => true,
+
+    // DocSearch credentials
+    'docsearchApiKey' => env('DOCSEARCH_KEY'),
+    'docsearchIndexName' => env('DOCSEARCH_INDEX'),
+];
+<<<<<<< HEAD
+=======
+];
+<<<<<<< HEAD
+>>>>>>> b4eb4c6 (.)
+=======
+];
+>>>>>>> e1c77f7 (Lint)
+=======
+>>>>>>> 285fa96 (.)
+=======
+];
+>>>>>>> d019e5d (Lint)
+>>>>>>> edba1131118fe43273e769e0d7e036e54ab0e8c9
+=======
+>>>>>>> 494a611dbac62dcd6cf37891fdaeccb7c3ac7f53
