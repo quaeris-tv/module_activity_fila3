e5a8a5cfb0d44f63baa5a91dd8c3fb51e4face13
marco76tv <marco.sottana@gmail.com>
2024-01-16 13:56:19 +0100

    up

--- Config/config.php
+++ Config/config.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 return [
     'name' => 'Activity',
     'navigation_sort' => 1,
-    'icon' => 'heroicon-o-cog', // icon on dashboard
+    'icon' => 'heroicon-o-clipboard-document-list', // icon on dashboard
 ];
