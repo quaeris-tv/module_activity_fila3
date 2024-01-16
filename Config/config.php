0bfae1530322d4afea7d5c2ad155ce1aaa2e40b2
marco76tv <marco.sottana@gmail.com>
2024-01-16 08:53:45 +0100

    rebase 1

--- Config/config.php
+++ Config/config.php
@@ -5,5 +5,8 @@ declare(strict_types=1);
 return [
     'name' => 'Activity',
     'navigation_sort' => 1,
+<<<<<<< HEAD
     'icon' => 'heroicon-o-cog', // icon on dashboard
+=======
+>>>>>>> ee7d88d (first)
 ];
