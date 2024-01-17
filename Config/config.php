a5411effec251ec02f2c9daaa73482e249067411
marco76tv <marco.sottana@gmail.com>
2024-01-17 15:58:43 +0100

    .

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 return [
     'name' => 'Activity',
     'navigation_sort' => 1,
+<<<<<<< HEAD
     'icon' => 'heroicon-o-cog', // icon on dashboard
     'navigation_sort' => 1,
+=======
+    'icon' => 'heroicon-o-clipboard-document-list', // icon on dashboard
+>>>>>>> f0ef646 (.)
 ];
