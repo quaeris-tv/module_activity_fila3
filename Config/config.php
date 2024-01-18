dc92603df74d8052e10be532205814300aec9774
marco76tv <marco.sottana@gmail.com>
2024-01-18 18:40:16 +0100

    .

--- Config/config.php
+++ Config/config.php
@@ -5,10 +5,6 @@ declare(strict_types=1);
 return [
     'name' => 'Activity',
     'navigation_sort' => 1,
-<<<<<<< HEAD
     'icon' => 'heroicon-o-clipboard-document-list', // icon on dashboard
-=======
-    'icon' => 'heroicon-o-cog', // icon on dashboard
-    'navigation_sort' => 1,
->>>>>>> 0c6fe67 (🔧 chore(.gitignore): remove unnecessary merge conflict markers in .gitignore file)
+
 ];
