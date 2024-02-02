47f534a2d97bad3474076176476289830219a22f
marco76tv <marco76tv@users.noreply.github.com>
2024-02-02 11:40:05 +0000

    Lint

--- Models/StoredEvent.php
+++ Models/StoredEvent.php
@@ -23,6 +23,7 @@ use Spatie\SchemalessAttributes\SchemalessAttributes;
  * @property string|null                                            $created_by
  * @property string|null                                            $updated_by
  * @property \Spatie\EventSourcing\StoredEvents\ShouldBeStored|null $event
+ *
  * @method static EloquentStoredEventQueryBuilder|StoredEvent afterVersion(int $version)
  * @method static EloquentStoredEventCollection<int, static>  all($columns = ['*'])
  * @method static EloquentStoredEventCollection<int, static>  get($columns = ['*'])
@@ -45,8 +46,10 @@ use Spatie\SchemalessAttributes\SchemalessAttributes;
  * @method static EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIs(string $property, ?mixed $value)
  * @method static EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIsNot(string $property, ?mixed $value)
  * @method static EloquentStoredEventQueryBuilder|StoredEvent whereUpdatedBy($value)
+ *
  * @property \Spatie\EventSourcing\StoredEvents\ShouldBeStored|null $event
  * @property SchemalessAttributes                                   $meta_data
+ *
  * @method static EloquentStoredEventQueryBuilder|StoredEvent                                                   afterVersion(int $version)
  * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
  * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
@@ -84,8 +87,9 @@ use Spatie\SchemalessAttributes\SchemalessAttributes;
  * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
  * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
  * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static> all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static> get($columns = ['*'])
+ * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
+ * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
+ *
  * @mixin \Eloquent
  */
 class StoredEvent extends SpatieStoredEvent
