b4ab2de6854d1c31991519aae0f4c72744c82ab9
marco76tv <marco.sottana@gmail.com>
2024-03-11 12:23:06 +0100

    rebase 20

--- docs/archived/bootstrap.php
+++ docs/archived/bootstrap.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 use App\Listeners\GenerateSitemap;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 /* @var \Illuminate\Container\Container $container */
 /* @var \TightenCo\Jigsaw\Events\EventBus $events */
 =======
 /** @var \Illuminate\Container\Container $container */
 /** @var \TightenCo\Jigsaw\Events\EventBus $events */
 >>>>>>> 5ca6ed5 (Inline constructor default to property, Add return type declarations, and Redirect route to toRoute helper)
+=======
+/* @var \Illuminate\Container\Container $container */
+/* @var \TightenCo\Jigsaw\Events\EventBus $events */
+>>>>>>> 3690197 (Lint)
 
 /*
  * You can run custom code at different stages of the build process by
