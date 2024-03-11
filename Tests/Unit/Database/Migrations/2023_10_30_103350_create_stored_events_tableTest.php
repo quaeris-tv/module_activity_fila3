95ea58f8ecf1173d214acd832a3b048a7d20aeb3
marco76tv <marco.sottana@gmail.com>
2024-03-11 12:23:05 +0100

    rebase 20

--- Tests/Unit/Database/Migrations/2023_10_30_103350_create_stored_events_tableTest.php
+++ Tests/Unit/Database/Migrations/2023_10_30_103350_create_stored_events_tableTest.php
@@ -1,9 +1,15 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 namespace Tests\Unit;
 
+=======
+namespace Tests\Unit;
+
+use CreateStoredEventsTable;
+>>>>>>> 6398a7f (up)
 use Tests\TestCase;
 
 /**
@@ -13,16 +19,34 @@ use Tests\TestCase;
  */
 final class CreateStoredEventsTableTest extends TestCase
 {
+<<<<<<< HEAD
     private \CreateStoredEventsTable $createStoredEventsTable;
 
+=======
+    private CreateStoredEventsTable $createStoredEventsTable;
+
+    /**
+     * {@inheritdoc}
+     */
+>>>>>>> 6398a7f (up)
     protected function setUp(): void
     {
         parent::setUp();
 
+<<<<<<< HEAD
         /* @todo Correctly instantiate tested object to use it. */
         $this->createStoredEventsTable = new \CreateStoredEventsTable();
     }
 
+=======
+        /** @todo Correctly instantiate tested object to use it. */
+        $this->createStoredEventsTable = new CreateStoredEventsTable();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+>>>>>>> 6398a7f (up)
     protected function tearDown(): void
     {
         parent::tearDown();
@@ -32,7 +56,11 @@ final class CreateStoredEventsTableTest extends TestCase
 
     public function testUp(): void
     {
+<<<<<<< HEAD
         /* @todo This test is incomplete. */
+=======
+        /** @todo This test is incomplete. */
+>>>>>>> 6398a7f (up)
         self::markTestIncomplete();
     }
 }
