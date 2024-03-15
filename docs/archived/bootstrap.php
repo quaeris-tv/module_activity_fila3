cdf236298cc0ebb87e46aba63c3a3efc34e293cd
marco76tv <marco.sottana@gmail.com>
2024-03-15 12:23:14 +0100

    chore(BaseModel.php): remove unnecessary comment and merge conflicting changes from different branches chore(StoredEvent.php): merge conflicting changes from different branches chore(bootstrap.php): remove unnecessary comments and merge conflicting changes from different branches

--- docs/archived/bootstrap.php
+++ docs/archived/bootstrap.php
@@ -4,13 +4,8 @@ declare(strict_types=1);
 
 use App\Listeners\GenerateSitemap;
 
-<<<<<<< HEAD
 /* @var \Illuminate\Container\Container $container */
 /* @var \TightenCo\Jigsaw\Events\EventBus $events */
-=======
-/** @var \Illuminate\Container\Container $container */
-/** @var \TightenCo\Jigsaw\Events\EventBus $events */
->>>>>>> 5ca6ed5 (Inline constructor default to property, Add return type declarations, and Redirect route to toRoute helper)
 
 /*
  * You can run custom code at different stages of the build process by
