5eb37768abe88508a9fc88017738c037ea265533
marco76tv <marco.sottana@gmail.com>
2024-03-15 12:06:13 +0100

    fix(ActivityServiceProviderTest.php): remove unused strict_types declaration fix(ActivityResourceTest.php): remove unused strict_types declaration fix(ActivityDatabaseSeederTest.php): remove unused strict_types declaration fix(CreateSnapshotsTableTest.php): remove unused strict_types declaration fix(CreateStoredEventsTableTest.php): remove unused strict_types declaration fix(CreateActivityTableTest.php): remove unused strict_types declaration feat(CreateActivityTableTest.php): add missing use statement for CreateActivityTable class feat(CreateStoredEventsTableTest.php): add missing use statement for CreateStoredEventsTable class feat(CreateSnapshotsTableTest.php): add missing use statement for CreateSnapshotsTable class

--- Tests/Unit/Filament/Resources/ActivityResourceTest.php
+++ Tests/Unit/Filament/Resources/ActivityResourceTest.php
@@ -1,7 +1,10 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
+=======
+>>>>>>> 6398a7f (up)
 namespace Modules\Activity\Tests\Unit\Filament\Resources;
 
 use Modules\Activity\Filament\Resources\ActivityResource;
@@ -16,14 +19,30 @@ final class ActivityResourceTest extends TestCase
 {
     private ActivityResource $activityResource;
 
+<<<<<<< HEAD
+=======
+    /**
+     * {@inheritdoc}
+     */
+>>>>>>> 6398a7f (up)
     protected function setUp(): void
     {
         parent::setUp();
 
+<<<<<<< HEAD
         /* @todo Correctly instantiate tested object to use it. */
         $this->activityResource = new ActivityResource();
     }
 
+=======
+        /** @todo Correctly instantiate tested object to use it. */
+        $this->activityResource = new ActivityResource();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+>>>>>>> 6398a7f (up)
     protected function tearDown(): void
     {
         parent::tearDown();
@@ -33,25 +52,41 @@ final class ActivityResourceTest extends TestCase
 
     public function testForm(): void
     {
+<<<<<<< HEAD
         /* @todo This test is incomplete. */
+=======
+        /** @todo This test is incomplete. */
+>>>>>>> 6398a7f (up)
         self::markTestIncomplete();
     }
 
     public function testTable(): void
     {
+<<<<<<< HEAD
         /* @todo This test is incomplete. */
+=======
+        /** @todo This test is incomplete. */
+>>>>>>> 6398a7f (up)
         self::markTestIncomplete();
     }
 
     public function testGetRelations(): void
     {
+<<<<<<< HEAD
         /* @todo This test is incomplete. */
+=======
+        /** @todo This test is incomplete. */
+>>>>>>> 6398a7f (up)
         self::markTestIncomplete();
     }
 
     public function testGetPages(): void
     {
+<<<<<<< HEAD
         /* @todo This test is incomplete. */
+=======
+        /** @todo This test is incomplete. */
+>>>>>>> 6398a7f (up)
         self::markTestIncomplete();
     }
 }
