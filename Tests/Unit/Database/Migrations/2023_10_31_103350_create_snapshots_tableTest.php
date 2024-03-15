4099edfd1d29810bb6a00ec188cb4c79f5fa9df1
marco76tv <marco.sottana@gmail.com>
2024-03-15 12:06:29 +0100

    test(ActivityDatabaseSeederTest.php): remove unused import statements and fix namespace test(CreateActivityTableTest.php): remove unused import statements and fix namespace test(CreateSnapshotsTableTest.php): remove unused import statements and fix namespace test(CreateStoredEventsTableTest.php): remove unused import statements and fix namespace test(ActivityResourceTest.php): remove unused import statements and fix namespace test(ActivityServiceProviderTest.php): remove unused import statements and fix namespace

--- Tests/Unit/Database/Migrations/2023_10_31_103350_create_snapshots_tableTest.php
+++ Tests/Unit/Database/Migrations/2023_10_31_103350_create_snapshots_tableTest.php
@@ -1,15 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 declare(strict_types=1);
 
 namespace Tests\Unit;
 
-=======
-namespace Tests\Unit;
-
-use CreateSnapshotsTable;
->>>>>>> 6398a7f (up)
 use Tests\TestCase;
 
 /**
@@ -19,34 +13,16 @@ use Tests\TestCase;
  */
 final class CreateSnapshotsTableTest extends TestCase
 {
-<<<<<<< HEAD
     private \CreateSnapshotsTable $createSnapshotsTable;
 
-=======
-    private CreateSnapshotsTable $createSnapshotsTable;
-
-    /**
-     * {@inheritdoc}
-     */
->>>>>>> 6398a7f (up)
     protected function setUp(): void
     {
         parent::setUp();
 
-<<<<<<< HEAD
         /* @todo Correctly instantiate tested object to use it. */
         $this->createSnapshotsTable = new \CreateSnapshotsTable();
     }
 
-=======
-        /** @todo Correctly instantiate tested object to use it. */
-        $this->createSnapshotsTable = new CreateSnapshotsTable();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
->>>>>>> 6398a7f (up)
     protected function tearDown(): void
     {
         parent::tearDown();
@@ -56,11 +32,7 @@ final class CreateSnapshotsTableTest extends TestCase
 
     public function testUp(): void
     {
-<<<<<<< HEAD
         /* @todo This test is incomplete. */
-=======
-        /** @todo This test is incomplete. */
->>>>>>> 6398a7f (up)
         self::markTestIncomplete();
     }
 }
