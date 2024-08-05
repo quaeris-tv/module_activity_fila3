39a4dadf059a2f1a4b7a4c4cd9b274a9a5fad985
marco <marco.sottana@gmail.com>
2024-08-05 12:43:44 +0200

    up

--- Filament/Resources/ActivityResource/Pages/ListActivities.php
+++ Filament/Resources/ActivityResource/Pages/ListActivities.php
@@ -7,10 +7,21 @@ namespace Modules\Activity\Filament\Resources\ActivityResource\Pages;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 use Modules\Activity\Filament\Resources\ActivityResource;
+use Modules\UI\Enums\TableLayoutEnum;
+use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 
 class ListActivities extends ListRecords
 {
     protected static string $resource = ActivityResource::class;
+
+    public TableLayoutEnum $layoutView = TableLayoutEnum::GRID;
+
+    protected function getTableHeaderActions(): array
+    {
+        return [
+            TableLayoutToggleTableAction::make(),
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
