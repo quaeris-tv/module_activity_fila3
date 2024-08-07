2d585490b71ca427cfd772851684c3db1513ede2
marco <marco.sottana@gmail.com>
2024-08-07 17:56:42 +0200

    up

---
+++ Filament/Resources/StoredEventResource/Pages/CreateStoredEvent.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Activity\Filament\Resources\StoredEventResource\Pages;
+
+use Modules\Activity\Filament\Resources\StoredEventResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateStoredEvent extends CreateRecord
+{
+    protected static string $resource = StoredEventResource::class;
+}
