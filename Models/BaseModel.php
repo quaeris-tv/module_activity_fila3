7e7e51f0d65163d77d6911ddb5b8e678edc0ae70
marco <marco.sottana@gmail.com>
2024-08-27 16:15:24 +0200

    up

--- Models/BaseModel.php
+++ Models/BaseModel.php
@@ -65,7 +65,7 @@ abstract class BaseModel extends Model
     /** @var string */
     protected $primaryKey = 'id';
 
-    /** @var array<int, string> */
+    /** @var list<string> */
     protected $hidden = [
         // 'password'
     ];
