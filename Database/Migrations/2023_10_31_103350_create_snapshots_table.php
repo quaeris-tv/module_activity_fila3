f0aa32b6f0a1b531b1e0344f457a24eb34ddbef5
marco <marco.sottana@gmail.com>
2024-09-12 09:03:32 +0200

    🔧 (gitignore): resolve conflict in .gitignore file 📝 (Database/Migrations): add new migration files for creating activity and stored events tables 📝 (Models/BaseModel): update casts method and add hidden property 📝 (Models/Snapshot): update model properties and methods 📝 (Providers/EventServiceProvider): resolve conflict in EventServiceProvider file 📝 (README.md): resolve conflict in README.md file 🔧 (composer.json): resolve conflict in composer.json file
    🔧 (package.json): update Vite dependency version to 5.4.1
    🔧 (WorkbenchServiceProvider.php, DatabaseSeeder.php, api.php, console.php, web.php): resolve merge conflicts and remove unnecessary code markers

--- Database/Migrations/2023_10_31_103350_create_snapshots_table.php
+++ Database/Migrations/2023_10_31_103350_create_snapshots_table.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+<<<<<<< HEAD
 // namespace Modules\Activity\Database\Migrations;
 
 use Illuminate\Database\Schema\Blueprint;
@@ -10,6 +11,13 @@ use Modules\Xot\Database\Migrations\XotBaseMigration;
 return new class extends XotBaseMigration
 {
     //protected ?string $model_class = Activity::class;
+=======
+use Illuminate\Database\Schema\Blueprint;
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+
+class CreateSnapshotsTable extends XotBaseMigration
+{
+>>>>>>> c72b996 (.)
     public function up(): void
     {
         // -- CREATE --
@@ -26,6 +34,7 @@ return new class extends XotBaseMigration
         // -- UPDATE --
         $this->tableUpdate(
             function (Blueprint $table): void {
+<<<<<<< HEAD
                 // if (! $this->hasColumn('created_by')) {
                 //    $table->string('created_by')->nullable();
                 //    $table->string('updated_by')->nullable();
@@ -35,3 +44,14 @@ return new class extends XotBaseMigration
         );
     }
 };
+=======
+                //if (! $this->hasColumn('created_by')) {
+                //    $table->string('created_by')->nullable();
+                //    $table->string('updated_by')->nullable();
+                //}
+                $this->updateTimestamps($table,false);
+            }
+        );
+    }
+}
+>>>>>>> c72b996 (.)
