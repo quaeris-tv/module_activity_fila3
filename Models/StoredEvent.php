c1924d58f04e6651eb6f2932bd132906a2eb89b0
marco <marco.sottana@gmail.com>
2024-09-17 17:37:05 +0200

    up

--- Models/StoredEvent.php
+++ Models/StoredEvent.php
@@ -5,121 +5,56 @@ declare(strict_types=1);
 namespace Modules\Activity\Models;
 
 use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent as SpatieStoredEvent;
-use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection;
-use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder;
-use Spatie\SchemalessAttributes\SchemalessAttributes;
 
 // use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 
 /**
  * Modules\Activity\Models\StoredEvent.
  *
- * @property int                                                    $id
- * @property string|null                                            $aggregate_uuid
- * @property int|null                                               $aggregate_version
- * @property int                                                    $event_version
- * @property string                                                 $event_class
- * @property array                                                  $event_properties
- * @property SchemalessAttributes                                   $meta_data
- * @property string                                                 $created_at
- * @property string|null                                            $created_by
- * @property string|null                                            $updated_by
- * @property \Spatie\EventSourcing\StoredEvents\ShouldBeStored|null $event
- *
- * @method static EloquentStoredEventQueryBuilder|StoredEvent afterVersion(int $version)
- * @method static EloquentStoredEventCollection<int, static>  all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>  get($columns = ['*'])
- * @method static EloquentStoredEventQueryBuilder|StoredEvent lastEvent(string ...$eventClasses)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent newModelQuery()
- * @method static EloquentStoredEventQueryBuilder|StoredEvent newQuery()
- * @method static EloquentStoredEventQueryBuilder|StoredEvent query()
- * @method static EloquentStoredEventQueryBuilder|StoredEvent startingFrom(int $storedEventId)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereAggregateRoot(string $uuid)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereAggregateUuid($value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereAggregateVersion($value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereCreatedAt($value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereCreatedBy($value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereEvent(string ...$eventClasses)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereEventClass($value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereEventProperties($value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereEventVersion($value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereId($value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereMetaData($value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIs(string $property, ?mixed $value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIsNot(string $property, ?mixed $value)
- * @method static EloquentStoredEventQueryBuilder|StoredEvent whereUpdatedBy($value)
- *
- * @property \Spatie\EventSourcing\StoredEvents\ShouldBeStored|null $event
- * @property SchemalessAttributes                                   $meta_data
- *
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|EloquentStoredEvent withMetaDataAttributes()
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    all($columns = ['*'])
- * @method static EloquentStoredEventCollection<int, static>                                                    get($columns = ['*'])
- *
+ * @property int $id
+ * @property string|null $aggregate_uuid
+ * @property int|null $aggregate_version
+ * @property int $event_version
+ * @property string $event_class
+ * @property array $event_properties
+ * @property \Spatie\SchemalessAttributes\SchemalessAttributes $meta_data
+ * @property string $created_at
+ * @property string|null $updated_by
+ * @property string|null $created_by
+ * @property-read \Spatie\EventSourcing\StoredEvents\ShouldBeStored|null $event
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent afterVersion(int $version)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> all($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> get($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent lastEvent(string ...$eventClasses)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent newModelQuery()
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent newQuery()
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent query()
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent startingFrom(int $storedEventId)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereAggregateRoot(string $uuid)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereAggregateUuid($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereAggregateVersion($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereCreatedAt($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereCreatedBy($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereEvent(string ...$eventClasses)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereEventClass($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereEventProperties($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereEventVersion($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereId($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereMetaData($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIs(string $property, ?mixed $value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIsNot(string $property, ?mixed $value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereUpdatedBy($value)
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent withMetaDataAttributes()
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> all($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> get($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> all($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> get($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> all($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> get($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> all($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> get($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> all($columns = ['*'])
+ * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> get($columns = ['*'])
  * @mixin \Eloquent
  */
 class StoredEvent extends SpatieStoredEvent
