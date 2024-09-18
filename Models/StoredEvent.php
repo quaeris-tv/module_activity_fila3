686dcd08bdb5f77e0a25d7ca7b97bac0116b9d80
marco76tv <marco.sottana@gmail.com>
2024-09-18 11:24:48 +0200

    up

--- Models/StoredEvent.php
+++ Models/StoredEvent.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace Modules\Activity\Models;
 
 use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent as SpatieStoredEvent;
-
-// use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Spatie\SchemalessAttributes\SchemalessAttributes;
+use Spatie\EventSourcing\StoredEvents\ShouldBeStored;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 
 /**
- * Modules\Activity\Models\StoredEvent.
+ * Class StoredEvent
+ *
+ * Represents a stored event in the activity module.
  *
  * @property int $id
  * @property string|null $aggregate_uuid
@@ -17,48 +21,63 @@ use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent as SpatieStored
  * @property int $event_version
  * @property string $event_class
  * @property array $event_properties
- * @property \Spatie\SchemalessAttributes\SchemalessAttributes $meta_data
+ * @property SchemalessAttributes $meta_data
  * @property string $created_at
  * @property string|null $updated_by
  * @property string|null $created_by
- * @property-read \Spatie\EventSourcing\StoredEvents\ShouldBeStored|null $event
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent afterVersion(int $version)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> all($columns = ['*'])
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventCollection<int, static> get($columns = ['*'])
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent lastEvent(string ...$eventClasses)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent newModelQuery()
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent newQuery()
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent query()
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent startingFrom(int $storedEventId)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereAggregateRoot(string $uuid)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereAggregateUuid($value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereAggregateVersion($value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereCreatedAt($value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereCreatedBy($value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereEvent(string ...$eventClasses)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereEventClass($value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereEventProperties($value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereEventVersion($value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereId($value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereMetaData($value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIs(string $property, ?mixed $value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent wherePropertyIsNot(string $property, ?mixed $value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent whereUpdatedBy($value)
- * @method static \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEventQueryBuilder|StoredEvent withMetaDataAttributes()
+ * @property-read ShouldBeStored|null $event
+ *
+ * @method static Builder|StoredEvent afterVersion(int $version)
+ * @method static Collection|StoredEvent[] all($columns = ['*'])
+ * @method static Collection|StoredEvent[] get($columns = ['*'])
+ * @method static Builder|StoredEvent lastEvent(string ...$eventClasses)
+ * @method static Builder|StoredEvent newModelQuery()
+ * @method static Builder|StoredEvent newQuery()
+ * @method static Builder|StoredEvent query()
+ * @method static Builder|StoredEvent startingFrom(int $storedEventId)
+ * @method static Builder|StoredEvent whereAggregateRoot(string $uuid)
+ * @method static Builder|StoredEvent whereAggregateUuid($value)
+ * @method static Builder|StoredEvent whereAggregateVersion($value)
+ * @method static Builder|StoredEvent whereCreatedAt($value)
+ * @method static Builder|StoredEvent whereCreatedBy($value)
+ * @method static Builder|StoredEvent whereEvent(string ...$eventClasses)
+ * @method static Builder|StoredEvent whereEventClass($value)
+ * @method static Builder|StoredEvent whereEventProperties($value)
+ * @method static Builder|StoredEvent whereEventVersion($value)
+ * @method static Builder|StoredEvent whereId($value)
+ * @method static Builder|StoredEvent whereMetaData($value)
+ * @method static Builder|StoredEvent wherePropertyIs(string $property, ?mixed $value)
+ * @method static Builder|StoredEvent wherePropertyIsNot(string $property, ?mixed $value)
+ * @method static Builder|StoredEvent whereUpdatedBy($value)
+ * @method static Builder|StoredEvent withMetaDataAttributes()
+ *
  * @mixin \Eloquent
  */
 class StoredEvent extends SpatieStoredEvent
 {
-    /**
-     * @var list<string>
-     */
-    protected $fillable = ['id', 'aggregate_uuid', 'aggregate_version', 'event_version', 'event_class', 'event_properties', 'meta_data', 'created_at', 'event_class', 'aggregate_uuid', 'created_at', 'updated_at'];
+    /** @var list<string> */
+    protected $fillable = [
+        'id',
+        'aggregate_uuid',
+        'aggregate_version',
+        'event_version',
+        'event_class',
+        'event_properties',
+        'meta_data',
+        'created_at',
+        'updated_by',
+        'created_by'
+    ];
+
+
+    // protected $attributes = [
+    //     'field_name' => '',
+    // ];
 
     /**
-     * @var list<string>
+     * The database connection used by the model.
+     *
+     * @var string
      */
-    protected $attributes = ['field_name'];
-
-    /** @var string */
     protected $connection = 'activity';
 }
