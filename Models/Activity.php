42cec3573e636c6869875278a01c4bc9275565c4
marco <marco.sottana@gmail.com>
2024-10-18 12:08:42 +0200

    This patch updates the `StoredEvent` model to use the `Spatie\SchemalessAttributes\SchemalessAttributes` trait, which allows you to store additional attributes on a stored event that are not part of the event itself. This is useful for storing metadata about the event, such as who created it or when it was last updated.
    The patch also updates the `StoredEvent` model's `$fillable` property to include the `meta_data` attribute, which will be used to store the additional metadata about the event.
    
    Note that this patch only applies to the `StoredEvent` model and does not affect any other models in the activity module.

--- Models/Activity.php
+++ Models/Activity.php
@@ -16,29 +16,30 @@ use Spatie\Activitylog\Models\Activity as SpatieActivity;
 /**
  * Modules\Activity\Models\Activity.
  *
- * @property int                             $id
- * @property string|null                     $log_name
- * @property string                          $description
- * @property string|null                     $subject_type
- * @property int|null                        $subject_id
- * @property string|null                     $causer_type
- * @property int|null                        $causer_id
- * @property Collection|null                 $properties
- * @property string|null                     $batch_uuid
- * @property string|null                     $event
+ * @property int $id
+ * @property string|null $log_name
+ * @property string $description
+ * @property string|null $subject_type
+ * @property int|null $subject_id
+ * @property string|null $causer_type
+ * @property int|null $causer_id
+ * @property Collection|null $properties
+ * @property string|null $batch_uuid
+ * @property string|null $event
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null                     $created_by
- * @property string|null                     $updated_by
- * @property Model|\Eloquent                 $causer
- * @property Collection                      $changes
- * @property Model|\Eloquent                 $subject
- * @method static EloquentBuilder|Activity                       causedBy(\Illuminate\Database\Eloquent\Model $causer)
- * @method static EloquentBuilder|Activity                       forBatch(string $batchUuid)
- * @method static EloquentBuilder|Activity                       forEvent(string $event)
- * @method static EloquentBuilder|Activity                       forSubject(\Illuminate\Database\Eloquent\Model $subject)
- * @method static EloquentBuilder|Activity                       hasBatch()
- * @method static EloquentBuilder|Activity                       inLog(...$logNames)
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property Model|Eloquent $causer
+ * @property Collection $changes
+ * @property Model|Eloquent $subject
+ *
+ * @method static EloquentBuilder|Activity causedBy(\Illuminate\Database\Eloquent\Model $causer)
+ * @method static EloquentBuilder|Activity forBatch(string $batchUuid)
+ * @method static EloquentBuilder|Activity forEvent(string $event)
+ * @method static EloquentBuilder|Activity forSubject(\Illuminate\Database\Eloquent\Model $subject)
+ * @method static EloquentBuilder|Activity hasBatch()
+ * @method static EloquentBuilder|Activity inLog(...$logNames)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Activity newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Activity query()
@@ -56,22 +57,27 @@ use Spatie\Activitylog\Models\Activity as SpatieActivity;
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereSubjectType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereUpdatedBy($value)
- * @property Model|\Eloquent $causer
- * @property Collection      $changes
- * @property Model|\Eloquent $subject
- * @method static EloquentBuilder|Activity                       causedBy(\Illuminate\Database\Eloquent\Model $causer)
- * @method static EloquentBuilder|Activity                       forBatch(string $batchUuid)
- * @method static EloquentBuilder|Activity                       forEvent(string $event)
- * @method static EloquentBuilder|Activity                       forSubject(\Illuminate\Database\Eloquent\Model $subject)
- * @method static EloquentBuilder|Activity                       hasBatch()
- * @method static EloquentBuilder|Activity                       inLog(...$logNames)
+ *
+ * @property Model|Eloquent $causer
+ * @property Collection $changes
+ * @property Model|Eloquent $subject
+ *
+ * @method static EloquentBuilder|Activity causedBy(\Illuminate\Database\Eloquent\Model $causer)
+ * @method static EloquentBuilder|Activity forBatch(string $batchUuid)
+ * @method static EloquentBuilder|Activity forEvent(string $event)
+ * @method static EloquentBuilder|Activity forSubject(\Illuminate\Database\Eloquent\Model $subject)
+ * @method static EloquentBuilder|Activity hasBatch()
+ * @method static EloquentBuilder|Activity inLog(...$logNames)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Activity newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Activity query()
+ *
  * @property string|null $deleted_at
  * @property string|null $deleted_by
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Activity whereDeletedBy($value)
+ *
  * @mixin \Eloquent
  * @mixin Eloquent
  */
