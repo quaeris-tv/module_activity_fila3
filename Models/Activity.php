161059255989b251e3964c1e77ee9047de7ac9df
marco <marco.sottana@gmail.com>
2024-11-04 11:11:31 +0100

    rebase 222

--- Models/Activity.php
+++ Models/Activity.php
@@ -1,22 +1,16 @@
 <?php
-/**
- * ---.
- */
 
 declare(strict_types=1);
 
 namespace Modules\Activity\Models;
 
-use Eloquent;
-use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Collection;
 use Spatie\Activitylog\Models\Activity as SpatieActivity;
 
 /**
- * Modules\Activity\Models\Activity.
+ * Class Activity.
  *
- 
+ * This class extends the Spatie Activity model to represent activities in the application.
+ *
  * @mixin \Eloquent
  */
 class Activity extends SpatieActivity
@@ -26,28 +20,32 @@ class Activity extends SpatieActivity
      *
      * @psalm-var list{'id', 'log_name', 'description', 'subject_type', 'event', 'subject_id', 'causer_type', 'causer_id', 'properties', 'batch_uuid', 'created_at', 'updated_at'}
      */
-    protected $fillable = ['id', 'log_name', 'description', 'subject_type', 'event', 'subject_id', 'causer_type', 'causer_id', 'properties', 'batch_uuid', 'created_at', 'updated_at'];
+    protected $fillable = [
+        'id',
+        'log_name',
+        'description',
+        'subject_type',
+        'event',
+        'subject_id',
+        'causer_type',
+        'causer_id',
+        'properties',
+        'batch_uuid',
+        'created_at',
+        'updated_at',
+    ];
 
-<<<<<<< HEAD
-    // /**
-    //  * @var array<string>
-    //  *
-    //  * @psalm-var list{'field_name'}
-    //  */
-    // protected $attributes = [
-    //     'field_name',
-    // ];
-=======
     /**
      * @var array<string>
      *
      * @psalm-var list{'field_name'}
      */
     protected $attributes = [
-        'field_name',
+        'field_name', // Consider adding real field names as needed
     ];
->>>>>>> 7352755 (up)
 
     /** @var string */
     protected $connection = 'activity';
+
+    // Additional methods or relationships can be defined here as needed
 }
