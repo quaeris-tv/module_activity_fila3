919882326d70d0e17f4f531f7d079d97a6d92f08
marco76tv <marco.sottana@gmail.com>
2024-12-04 12:31:08 +0100

    .

--- Filament/Resources/ActivityResource/Pages/ListActivities.php
+++ Filament/Resources/ActivityResource/Pages/ListActivities.php
@@ -5,27 +5,22 @@ declare(strict_types=1);
 namespace Modules\Activity\Filament\Resources\ActivityResource\Pages;
 
 use Filament\Tables;
-use Modules\UI\Enums\TableLayoutEnum;
-use Filament\Resources\Pages\ListRecords;
-use Modules\Xot\Filament\Traits\HasXotTable;
-use Modules\Xot\Filament\Pages\XotBaseListRecords;
 use Modules\Activity\Filament\Resources\ActivityResource;
+use Modules\Xot\Filament\Pages\XotBaseListRecords;
 
 class ListActivities extends XotBaseListRecords
 {
     protected static string $resource = ActivityResource::class;
 
-
     public function getListTableColumns(): array
     {
         return [
             Tables\Columns\TextColumn::make('id')
-                
+
                 ->sortable(),
-            Tables\Columns\TextColumn::make('description')
-                ,
+            Tables\Columns\TextColumn::make('description'),
             Tables\Columns\TextColumn::make('created_at')
-                
+
                 ->sortable(),
         ];
     }
