188e8c56846f81a4ca378b494742e0fcb4067aa7
marco76tv <marco.sottana@gmail.com>
2024-12-27 21:37:48 +0100

    up

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -8,13 +8,8 @@ use Modules\Xot\Providers\XotBaseRouteServiceProvider;
 
 class RouteServiceProvider extends XotBaseRouteServiceProvider
 {
-    /**
-     * The module namespace to assume when generating URLs to actions.
-     */
     protected string $moduleNamespace = 'Modules\Activity\Http\Controllers';
-
     protected string $module_dir = __DIR__;
-
     protected string $module_ns = __NAMESPACE__;
     public string $name = 'Activity';
 }
