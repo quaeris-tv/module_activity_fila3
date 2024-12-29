128bc8728ba4253fb4c99fc95bb416bb0c183d8c
marco76tv <marco.sottana@gmail.com>
2024-12-29 11:41:39 +0100

    up

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,13 +7,13 @@
         <title>Module Activity</title>
 
        {{-- Laravel Vite - CSS File --}}
-       {{-- {{ module_vite('build-activity', 'Resources/assets/sass/app.scss') }} --}}
+       {{-- {{ module_vite('build-activity', 'resources/assets/sass/app.scss') }} --}}
 
     </head>
     <body>
         @yield('content')
 
         {{-- Laravel Vite - JS File --}}
-        {{-- {{ module_vite('build-activity', 'Resources/assets/js/app.js') }} --}}
+        {{-- {{ module_vite('build-activity', 'resources/assets/js/app.js') }} --}}
     </body>
 </html>
