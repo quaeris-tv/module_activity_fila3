fbbff848e356f0213bbda19a8b67c55e028de68c
marco <marco.sottana@gmail.com>
2025-01-09 16:34:29 +0100

    up

--- app/Filament/Resources/SnapshotResource/Pages/ListSnapshots.php
+++ app/Filament/Resources/SnapshotResource/Pages/ListSnapshots.php
@@ -6,8 +6,33 @@ namespace Modules\Activity\Filament\Resources\SnapshotResource\Pages;
 
 use Modules\Activity\Filament\Resources\SnapshotResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ViewColumn;
 
 class ListSnapshots extends XotBaseListRecords
 {
     protected static string $resource = SnapshotResource::class;
+
+    public function getListTableColumns(): array
+    {
+        return [
+            'id' => TextColumn::make('id')
+                ->sortable(),
+            'aggregate_uuid' => TextColumn::make('aggregate_uuid')
+                ->searchable()
+                ->sortable()
+                ->wrap(),
+            'aggregate_version' => TextColumn::make('aggregate_version')
+                ->numeric()
+                ->sortable(),
+            'state' => ViewColumn::make('state')
+                ->view('activity::filament.tables.columns.state'),
+            'created_at' => TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable(),
+            'updated_at' => TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable(),
+        ];
+    }
 }
