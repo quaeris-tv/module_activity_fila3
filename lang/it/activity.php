931d2e25085c04c5ece7779e49b249d876780644
marco76tv <marco.sottana@gmail.com>
2025-01-20 22:36:21 +0100

    up

---
+++ lang/it/activity.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'navigation' => 
+  array (
+    'label' => 'activity.navigation',
+    'group' => 'activity.navigation',
+    'icon' => 'activity.navigation',
+    'sort' => 3,
+  ),
+);
---
+++ lang/it/stored_event.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'navigation' => 
+  array (
+    'label' => 'stored event.navigation',
+    'group' => 'stored event.navigation',
+    'icon' => 'stored event.navigation',
+    'sort' => 14,
+  ),
+);
