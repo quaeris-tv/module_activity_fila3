3c6691122f854f2f3e52b2564c009aa2532029b9
marco76tv <marco.sottana@gmail.com>
2025-01-20 22:36:21 +0100

    up

---
+++ lang/it/snapshot.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'navigation' => 
+  array (
+    'label' => 'snapshot.navigation',
+    'group' => 'snapshot.navigation',
+    'icon' => 'snapshot.navigation',
+    'sort' => 65,
+  ),
+);
