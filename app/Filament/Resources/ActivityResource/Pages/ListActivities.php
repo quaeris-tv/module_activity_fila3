21cdca4e291f77be2e046639470bfedea24ff84b
marco76tv <marco.sottana@gmail.com>
2025-02-03 22:03:02 +0100

    Auto update submodule

--- app/Filament/Resources/ActivityResource/Pages/ListActivities.php
+++ app/Filament/Resources/ActivityResource/Pages/ListActivities.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 declare(strict_types=1);
 
 namespace Modules\Activity\Filament\Resources\ActivityResource\Pages;
@@ -8,10 +7,6 @@ namespace Modules\Activity\Filament\Resources\ActivityResource\Pages;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Activity\Filament\Resources\ActivityResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
-<<<<<<< HEAD
-=======
-
->>>>>>> f51fd34 (up)
 
 /**
  * @see ActivityResource
@@ -23,7 +18,6 @@ class ListActivities extends XotBaseListRecords
     public function getListTableColumns(): array
     {
         return [
-<<<<<<< HEAD
             'id' => TextColumn::make('id')
                 ->sortable()
                 ->label('ID'),
@@ -52,34 +46,7 @@ class ListActivities extends XotBaseListRecords
                 ->sortable()
                 ->dateTime()
                 ->label('Created At'),
-=======
-            'id' => Tables\Columns\TextColumn::make('id')
-                ->sortable(),
-            'description' => Tables\Columns\TextColumn::make('description'),
-            'created_at' => Tables\Columns\TextColumn::make('created_at')
-                ->sortable(),
-=======
-namespace App\Http\Livewire\Activity\Filament\Resources\ActivityResource\Pages;
 
-use Filament\Tables\Columns\TextColumn;
-
-class ListActivities extends \Filament\Resources\Pages\ListRecords
-{
-    /**
-     * @return array<string, \Filament\Tables\Columns\Column>
-     */
-    public function getListTableColumns(): array
-    {
-        return [
-            'id' => TextColumn::make('id'),
-            'description' => TextColumn::make('description'),
-            'subject_type' => TextColumn::make('subject_type'),
-            'subject_id' => TextColumn::make('subject_id'),
-            'causer_type' => TextColumn::make('causer_type'),
-            'causer_id' => TextColumn::make('causer_id'),
-            'created_at' => TextColumn::make('created_at'),
->>>>>>> 36aa08e (up)
->>>>>>> origin/dev
         ];
     }
 }
