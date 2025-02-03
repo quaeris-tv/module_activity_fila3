c6a9aadad18aa8d447cfc05def830e900cceec10
marco76tv <marco.sottana@gmail.com>
2025-02-03 15:49:55 +0100

    Auto update submodule

--- app/Filament/Resources/StoredEventResource.php
+++ app/Filament/Resources/StoredEventResource.php
@@ -5,16 +5,14 @@ declare(strict_types=1);
 namespace Modules\Activity\Filament\Resources;
 
 use Filament\Forms\Form;
-<<<<<<< HEAD
-use Modules\Activity\Filament\Resources\StoredEventResource\Pages;
-use Modules\Activity\Models\StoredEvent;
-use Modules\Xot\Filament\Resources\XotBaseResource;
-=======
 use Filament\Resources\Resource;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\KeyValue;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\DateTimePicker;
 use Modules\Activity\Models\StoredEvent;
 use Modules\Xot\Filament\Resources\XotBaseResource;
 use Modules\Activity\Filament\Resources\StoredEventResource\Pages;
->>>>>>> 36aa08e (up)
 
 class StoredEventResource extends XotBaseResource
 {
@@ -22,31 +20,45 @@ class StoredEventResource extends XotBaseResource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-<<<<<<< HEAD
-    public static function getFormSchema(): array
-    {
-        return [
-            \Filament\Forms\Components\TextInput::make('event_class')
-                ->required()
-                ->maxLength(255),
-            \Filament\Forms\Components\KeyValue::make('event_properties')
-                ->columnSpanFull(),
-            \Filament\Forms\Components\TextInput::make('aggregate_uuid')
-                ->maxLength(36),
-            \Filament\Forms\Components\TextInput::make('aggregate_version')
-                ->numeric(),
-            \Filament\Forms\Components\Textarea::make('meta_data')
-                ->columnSpanFull(),
-            \Filament\Forms\Components\DateTimePicker::make('created_at')
-                ->required(),
-        ];
-=======
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
+                TextInput::make('event_class')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Event Class')
+                    ->placeholder('Enter event class')
+                    ->helperText('The class name of the event'),
+
+                KeyValue::make('event_properties')
+                    ->columnSpanFull()
+                    ->label('Event Properties')
+                    ->helperText('Properties associated with this event'),
+
+                TextInput::make('aggregate_uuid')
+                    ->maxLength(36)
+                    ->label('Aggregate UUID')
+                    ->placeholder('Enter aggregate UUID')
+                    ->helperText('UUID of the aggregate (optional)'),
+
+                TextInput::make('aggregate_version')
+                    ->numeric()
+                    ->label('Aggregate Version')
+                    ->placeholder('Enter aggregate version')
+                    ->helperText('Version number of the aggregate'),
+
+                Textarea::make('meta_data')
+                    ->columnSpanFull()
+                    ->label('Meta Data')
+                    ->placeholder('Enter meta data')
+                    ->helperText('Additional metadata for this event'),
+
+                DateTimePicker::make('created_at')
+                    ->required()
+                    ->label('Created At')
+                    ->helperText('When this event was created')
             ]);
->>>>>>> 36aa08e (up)
     }
 
     public static function getRelations(): array
