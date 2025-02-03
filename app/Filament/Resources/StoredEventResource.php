ef95d1b5262e01718ba00324827ba0defa86b164
marco76tv <marco.sottana@gmail.com>
2025-02-03 20:51:22 +0100

    Auto update submodule

--- app/Filament/Resources/StoredEventResource.php
+++ app/Filament/Resources/StoredEventResource.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Filament\Resources;
 
-use Filament\Forms\Form;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\KeyValue;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms;
 use Modules\Activity\Models\StoredEvent;
 use Modules\Xot\Filament\Resources\XotBaseResource;
 use Modules\Activity\Filament\Resources\StoredEventResource\Pages;
@@ -17,42 +13,40 @@ class StoredEventResource extends XotBaseResource
 {
     protected static ?string $model = StoredEvent::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
     public static function getFormSchema(): array
     {
         return [
-            TextInput::make('event_class')
+            'event_class' => Forms\Components\TextInput::make('event_class')
                 ->required()
                 ->maxLength(255)
                 ->label('Event Class')
                 ->placeholder('Enter event class')
                 ->helperText('The class name of the event'),
 
-            KeyValue::make('event_properties')
+            'event_properties' => Forms\Components\KeyValue::make('event_properties')
                 ->columnSpanFull()
                 ->label('Event Properties')
                 ->helperText('Properties associated with this event'),
 
-            TextInput::make('aggregate_uuid')
+            'aggregate_uuid' => Forms\Components\TextInput::make('aggregate_uuid')
                 ->maxLength(36)
                 ->label('Aggregate UUID')
                 ->placeholder('Enter aggregate UUID')
                 ->helperText('UUID of the aggregate (optional)'),
 
-            TextInput::make('aggregate_version')
+            'aggregate_version' => Forms\Components\TextInput::make('aggregate_version')
                 ->numeric()
                 ->label('Aggregate Version')
                 ->placeholder('Enter aggregate version')
                 ->helperText('Version number of the aggregate'),
 
-            Textarea::make('meta_data')
+            'meta_data' => Forms\Components\Textarea::make('meta_data')
                 ->columnSpanFull()
                 ->label('Meta Data')
                 ->placeholder('Enter meta data')
                 ->helperText('Additional metadata for this event'),
 
-            DateTimePicker::make('created_at')
+            'created_at' => Forms\Components\DateTimePicker::make('created_at')
                 ->required()
                 ->label('Created At')
                 ->helperText('When this event was created')
