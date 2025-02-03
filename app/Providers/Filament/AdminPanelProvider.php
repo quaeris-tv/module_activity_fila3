a33a6626c6f2040a447eb5132dc3b81945c160b2
marco76tv <marco.sottana@gmail.com>
2025-02-03 16:00:42 +0100

    .

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -14,6 +14,7 @@ class AdminPanelProvider extends XotBasePanelProvider
     public function panel(Panel $panel): Panel
     {
         $panel = parent::panel($panel);
+        //---
         return $panel;
     }
 }
