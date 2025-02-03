e9ce567e94c78eb41e97a9d2f4655e7e803592ef
marco76tv <marco.sottana@gmail.com>
2025-02-03 18:53:23 +0100

    up

--- app/Filament/Resources/StoredEventResource.php
+++ app/Filament/Resources/StoredEventResource.php
@@ -20,45 +20,44 @@ class StoredEventResource extends XotBaseResource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
+    public static function getFormSchema(): array
     {
-        return $form
-            ->schema([
-                TextInput::make('event_class')
-                    ->required()
-                    ->maxLength(255)
-                    ->label('Event Class')
-                    ->placeholder('Enter event class')
-                    ->helperText('The class name of the event'),
+        return [
+            TextInput::make('event_class')
+                ->required()
+                ->maxLength(255)
+                ->label('Event Class')
+                ->placeholder('Enter event class')
+                ->helperText('The class name of the event'),
 
-                KeyValue::make('event_properties')
-                    ->columnSpanFull()
-                    ->label('Event Properties')
-                    ->helperText('Properties associated with this event'),
+            KeyValue::make('event_properties')
+                ->columnSpanFull()
+                ->label('Event Properties')
+                ->helperText('Properties associated with this event'),
 
-                TextInput::make('aggregate_uuid')
-                    ->maxLength(36)
-                    ->label('Aggregate UUID')
-                    ->placeholder('Enter aggregate UUID')
-                    ->helperText('UUID of the aggregate (optional)'),
+            TextInput::make('aggregate_uuid')
+                ->maxLength(36)
+                ->label('Aggregate UUID')
+                ->placeholder('Enter aggregate UUID')
+                ->helperText('UUID of the aggregate (optional)'),
 
-                TextInput::make('aggregate_version')
-                    ->numeric()
-                    ->label('Aggregate Version')
-                    ->placeholder('Enter aggregate version')
-                    ->helperText('Version number of the aggregate'),
+            TextInput::make('aggregate_version')
+                ->numeric()
+                ->label('Aggregate Version')
+                ->placeholder('Enter aggregate version')
+                ->helperText('Version number of the aggregate'),
 
-                Textarea::make('meta_data')
-                    ->columnSpanFull()
-                    ->label('Meta Data')
-                    ->placeholder('Enter meta data')
-                    ->helperText('Additional metadata for this event'),
+            Textarea::make('meta_data')
+                ->columnSpanFull()
+                ->label('Meta Data')
+                ->placeholder('Enter meta data')
+                ->helperText('Additional metadata for this event'),
 
-                DateTimePicker::make('created_at')
-                    ->required()
-                    ->label('Created At')
-                    ->helperText('When this event was created')
-            ]);
+            DateTimePicker::make('created_at')
+                ->required()
+                ->label('Created At')
+                ->helperText('When this event was created')
+        ];
     }
 
     public static function getRelations(): array
