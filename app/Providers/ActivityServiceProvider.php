4f8f8296094af3dbb62c90b844abade0290369f9
marco76tv <marco.sottana@gmail.com>
2025-02-03 12:50:46 +0100

    up

--- app/Providers/ActivityServiceProvider.php
+++ app/Providers/ActivityServiceProvider.php
@@ -6,22 +6,9 @@ namespace Modules\Activity\Providers;
 
 use Modules\Xot\Providers\XotBaseServiceProvider;
 
-// use Modules\Xot\Services\BladeService;
-
-/**
- * Undocumented class.
- */
 class ActivityServiceProvider extends XotBaseServiceProvider
 {
-<<<<<<< HEAD
-<<<<<<< HEAD:app/Providers/ActivityServiceProvider.php
     public string $name = 'Activity';
-=======
-    public string $name = 'activity';
->>>>>>> 5b3e4a8 (up):Providers/ActivityServiceProvider.php
-=======
-    public string $name = 'Activity';
->>>>>>> 1350557 (up)
 
     protected string $module_dir = __DIR__;
 
@@ -30,36 +17,12 @@ class ActivityServiceProvider extends XotBaseServiceProvider
     public function boot(): void
     {
         parent::boot();
-        // ...
-        // activity()->log('boot');
-        // Event::listen('*', function (string $eventName, array $data) {
-        // dddx(['eventName' => $eventName, 'data' => $data]);
-        // ...
-        // activity()->log($eventName);
-        /*
-        try {
-            activity()
-                // ->performedOn($anEloquentModel)
-                ->causedBy(auth()->user)
-                ->withProperties($data)
-                ->log($eventName);
-        } catch (\Exception $e) {
-            activity()->causedBy(auth()->user)
-            ->withProperties(['message' => $e->getMessage()])
-            ->log($e->getMessage());
-        }
-        */
-        // });
+        // Additional boot logic can be added here
     }
 
     public function register(): void
     {
         parent::register();
-        // ...
-        // activity()->log('register');
-        // Event::listen('*', function (string $eventName, array $data) {
-        //    dddx(['eventName' => $eventName, 'data' => $data]);
-        //    // ...
-        // });
+        // Additional register logic can be added here
     }
 }
