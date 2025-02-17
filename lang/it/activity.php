25a68bdc41ae19ece2b65c5fb0eb5d3397ffb2a3
marco76tv <marco.sottana@gmail.com>
2025-02-17 19:37:51 +0100

    Auto update submodule

--- lang/it/activity.php
+++ lang/it/activity.php
@@ -1,9 +1,78 @@
-<?php return array (
-  'navigation' => 
-  array (
-    'label' => 'activity.navigation',
-    'group' => 'activity.navigation',
-    'icon' => 'activity.navigation',
-    'sort' => 7,
-  ),
-);
+<?php return array (
+    'navigation' => array (
+        'name' => 'Attività',
+        'plural' => 'Attività',
+        'group' => array (
+            'name' => 'Monitoraggio',
+            'description' => 'Monitoraggio delle attività di sistema',
+        ),
+        'label' => 'Attività',
+        'sort' => 60,
+        'icon' => 'activity-activity-animated',
+    ),
+    'fields' => array (
+        'user' => array (
+            'label' => 'Utente',
+            'name' => 'Nome',
+            'email' => 'Email',
+            'role' => 'Ruolo',
+        ),
+        'action' => array (
+            'label' => 'Azione',
+            'created' => 'Creato',
+            'updated' => 'Modificato',
+            'deleted' => 'Eliminato',
+            'viewed' => 'Visualizzato',
+            'downloaded' => 'Scaricato',
+            'uploaded' => 'Caricato',
+            'logged_in' => 'Accesso',
+            'logged_out' => 'Uscita',
+        ),
+        'subject' => array (
+            'label' => 'Oggetto',
+            'type' => 'Tipo',
+            'id' => 'ID',
+            'name' => 'Nome',
+        ),
+        'description' => 'Descrizione',
+        'ip_address' => 'Indirizzo IP',
+        'user_agent' => 'User Agent',
+        'created_at' => 'Data',
+        'properties' => array (
+            'label' => 'Proprietà',
+            'old' => 'Vecchio Valore',
+            'new' => 'Nuovo Valore',
+        ),
+    ),
+    'filters' => array (
+        'user' => 'Utente',
+        'action' => 'Azione',
+        'subject_type' => 'Tipo Oggetto',
+        'date_range' => 'Intervallo Date',
+        'ip_address' => 'Indirizzo IP',
+    ),
+    'actions' => array (
+        'view_details' => 'Visualizza Dettagli',
+        'export' => 'Esporta',
+        'clear_old' => 'Pulisci Vecchie',
+    ),
+    'messages' => array (
+        'no_activities' => 'Nessuna attività trovata',
+        'cleared' => 'Attività vecchie eliminate con successo',
+        'exported' => 'Attività esportate con successo',
+    ),
+    'export' => array (
+        'formats' => array (
+            'csv' => 'CSV',
+            'excel' => 'Excel',
+            'pdf' => 'PDF',
+        ),
+        'columns' => array (
+            'date' => 'Data',
+            'user' => 'Utente',
+            'action' => 'Azione',
+            'subject' => 'Oggetto',
+            'ip' => 'IP',
+        ),
+    ),
+);
