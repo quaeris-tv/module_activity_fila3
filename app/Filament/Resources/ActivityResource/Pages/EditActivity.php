49560e8e7a510e9e1c3439d27dd9faed4ea7704d
marco76tv <marco.sottana@gmail.com>
2025-02-26 15:45:04 +0100

    Auto update submodule

--- app/Filament/Resources/ActivityResource/Pages/EditActivity.php
+++ app/Filament/Resources/ActivityResource/Pages/EditActivity.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Modules\Activity\Filament\Resources\ActivityResource\Pages;
 
 use Filament\Actions\DeleteAction;
-use Filament\Resources\Pages\EditRecord;
 use Modules\Activity\Filament\Resources\ActivityResource;
 
 class EditActivity extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
--- app/Filament/Resources/StoredEventResource/Pages/CreateStoredEvent.php
+++ app/Filament/Resources/StoredEventResource/Pages/CreateStoredEvent.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Filament\Resources\StoredEventResource\Pages;
 
-use Filament\Resources\Pages\CreateRecord;
 use Modules\Activity\Filament\Resources\StoredEventResource;
 
 class CreateStoredEvent extends \Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord
